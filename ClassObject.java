619c45356b845e5a5b950a6ac1c893b143448187
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-05-14 19:17:22 +0530

    AddFile

---
+++ ClassObject.java
@@ -0,0 +1,12 @@
+class abc{
+	int a,b;
+}
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		abc o=new abc();
+		o.a=5;
+		o.b=10;
+		System.out.println(o.a+o.b);
+	}
