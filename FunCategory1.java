6a991b16928663bce39aac87d5b6a61853de5261
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-05-14 19:20:34 +0530

    AddFile

---
+++ FunCategory1.java
@@ -0,0 +1,15 @@
+class abc{
+	public void fun() {
+	int a=4;
+	System.out.println(a*a);
+}
+}
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		abc o=new abc();
+		o.fun();
+	}
+
+}
