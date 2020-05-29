6b2400708b2ad4e9e1f172fbdc6b65b14da496d0
Naren Purushothaman <65179572+narenoffl@users.noreply.github.com>
2020-05-29 13:13:13 +0530

    WiproCommandPrompt1

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome " + args[0]);
+
+	}
+
+}  
+
+
+Output: javac Assignment2.java
+        java Assignment2 Robert
+        Welcome Robert
